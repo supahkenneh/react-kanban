7f40501302fe4018c6bd5c8a561781fe0c6e9d54
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-15 09:21:12 -1000

    queue renders

--- src/components/Queue/index.js
+++ src/components/Queue/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import QueueCard from '../QueueCard';
 
 const Queue = props => {
+  console.log('queue: ', props);
   return (
     <div id="queue_container">
       <ul>
         {
           props.cards.map((card, idx) => {
             if (card.status === 'queue') {
-              return <QueueCard key={idx} title={card.title} body={card.body} />
+              //gives accessible properties to QueueCard
+              return <QueueCard 
+                key={idx} 
+                title={card.title} 
+                body={card.body} 
+                priority={card.priority} 
+                created_by={card.created_by} 
+                assigned_to={card.assigned_to}
+                />
             }
           })
         }
--- src/components/QueueCard/index.js
+++ src/components/QueueCard/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const QueueCard = (props) => {
-  console.log(props);
+  console.log('queueCard: ', props);
   return (
     <li>
-      <div>{props.title}</div>
-      <div>{props.body}</div>
+      <div className="task_card">
+        <p id="task_title">{props.title}</p>
+        <p id="task_body">{props.body}</p>
+        <p id="task_giver">{props.created_by}</p>
+        <p id="priority">{props.priority}</p>
+        <p id="tasked_to">{props.assigned_to}</p>
+      </div>
     </li>
   )
 }
