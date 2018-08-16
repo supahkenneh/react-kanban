b0b0cc8971dab4faa941d466262da89760f5fed2
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-15 18:51:14 -1000

    cleaned up

--- src/components/Column/index.js
+++ src/components/Column/index.js
@@ -15,33 +15,4 @@ const Column = props => {
   );
 };
 
-function filterCards(props) {
-  switch (props.status) {
-    case 'queue':
-      
-      break;
-  
-    default:
-      break;
-  }
-}
-
 export default Column;
-
-// import React from 'react';
-// import CardList from '../CardList';
-
-// const Column = props => {
-//   return (
-//     <div className="column">
-//       <div className="column_header">
-//         {props.label}
-//       </div>
-//       <div className="column_body">
-//         <CardList cards={props.cards} />
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Column;
