345a85e1a95e9675624bb1749cc568822f03d5fe
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-16 12:04:41 -1000

    able to get value from drop down menu

--- src/components/AddCardForm/addbutton.js
+++ src/components/AddCardForm/addbutton.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const NewCardButton = props => {
+  console.log(props);
   return (
     <button>Add Task</button>
   )
--- src/components/AddCardForm/index.js
+++ src/components/AddCardForm/index.js
@@ -1,54 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
 import NewCardButton from './addbutton';
 
-const newCardForm = props => {
-  const {
-    titleInput,
-    bodyInput,
-    priorityInput,
-    createInput,
-    assignedInput
-  } = props;
+class newCardForm extends Component {
+  constructor(props) {
+    super(props)
 
-  return (
-    <div className="newCard-container">
-      <label htmlFor="title">Title: </label>
-      <input
-        type="text"
-        name="title"
-        id="title"
-        value={titleInput}
-      />
-      <label htmlFor="body">Body: </label>
-      <input
-        type="text"
-        name="body"
-        id="body"
-        value={bodyInput}
-      />
-      <label htmlFor="priority">Priority: </label>
-      <select name="priority" id="priority">
-        <option value={priorityInput}> High </option>
-        <option value={priorityInput}> Med </option>
-        <option value={priorityInput}> Low </option>
-      </select>
-      <label htmlFor="created">Created By: </label>
-      <input
-        type="text"
-        name="created"
-        id="created"
-        value={createInput}
-      />
-      <label htmlFor="assign">Assign To: </label>
-      <input
-        type="text"
-        name="assign"
-        id="assign"
-        value={assignedInput}
-      />
-      <NewCardButton />
-    </div>
-  )
+    this.state = {
+      titleInput: '',
+      bodyInput: '',
+      priorityInput: 'High',
+      createInput: '',
+      assignedInput: ''
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    console.log('event ', event.target);
+    switch (event.target.id) {
+      case 'title':
+        this.setState({ titleInput: event.target.value })
+        break;
+      case 'body':
+        this.setState({ bodyInput: event.target.value })
+        break;
+      case 'priority':
+        this.setState({ priorityInput: event.target.value })
+        break;
+      case 'created':
+        this.setState({ createInput: event.target.value })
+        break;
+      case 'assign': 
+        this.setState({ assignedInput: event.target.value })
+        break;
+      default:
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <div className="newCard-container">
+        <label htmlFor="title">Title: </label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          value={this.state.titleInput}
+          onChange={this.handleInputChange}
+        />
+        <label htmlFor="body">Body: </label>
+        <input
+          type="text"
+          name="body"
+          id="body"
+          value={this.state.bodyInput}
+          onChange={this.handleInputChange}
+        />
+        <label htmlFor="priority">Priority: </label>
+        <select name="priority" id="priority" 
+          value={this.state.priorityInput}
+          onChange={this.handleInputChange}
+          >
+          <option> High </option>
+          <option> Med </option>
+          <option> Low </option>
+        </select>
+        <label htmlFor="created">Created By: </label>
+        <input
+          type="text"
+          name="created"
+          id="created"
+          value={this.createInput}
+          onChange={this.handleInputChange}
+        />
+        <label htmlFor="assign">Assign To: </label>
+        <input
+          type="text"
+          name="assign"
+          id="assign"
+          value={this.assignedInput}
+          onChange={this.handleInputChange}
+        />
+        <NewCardButton />
+      </div>
+    )
+  }
 }
 
 export default newCardForm;
